f38eaa3e2ffc53e4ad9e51b144e222a496a7c640
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-05-22 10:39:58 +0530

    Add Developer Class for Java

---
+++ others/Developer.java
@@ -0,0 +1,53 @@
+public class Developer {
+
+    private String name;
+    private int age;
+
+    public Developer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Developer developer = (Developer) o;
+
+        if (age != developer.age) return false;
+        return name != null ? name.equals(developer.name) : developer.name == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
